20e17eedaf5c316df2ef15c4fc77ed913c40be6a
bobrKryt120 <artemartem141012@gmail.com>
2026-05-17 11:10:54 +0300

    Update and rename java to lesson.js

---
+++ lesson.js
@@ -0,0 +1,25 @@
+const backPeach = document.querySelector(".peach")
+const leave = document.querySelector(".leave_from_me");
+const yes = document.querySelector(".yes");
+const no = document.querySelector(".no");
+const next = document.querySelector(".next_button")
+
+backPeach.addEventListener("click", () => {
+    if (leave.style.display === "block") {
+        leave.style.display = "none";
+    } else {
+        leave.style.display = "block";
+    }
+});
+
+yes.addEventListener("click", () => {
+    window.location.href = "second_page.html";
+});
+
+no.addEventListener("click", () => {
+    leave.style.display = "none";
+});
+
+next.addEventListener("click", () => {
+    window.location.href = "second_lesson.html";
+});
